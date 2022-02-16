729b0f0ad967e410cb00b252464083f876cc4b8b
Vladislav Sotin <sotin.vladislav@mail.ru>
2022-02-16 14:28:09 +0300

    bind tab layout

--- app/src/main/java/com/vlad/finboard/tab/IncomeFragment.kt
+++ app/src/main/java/com/vlad/finboard/tab/IncomeFragment.kt
@@ -5,17 +5,17 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.vlad.finboard.R
-import com.vlad.finboard.databinding.FragmentCostsBinding
+import com.vlad.finboard.databinding.FragmentIncomeBinding
 import com.vlad.finboard.navigation.navigate
 import com.vlad.finboard.navigation.screen.FragmentScreen
 
-class CostsFragment: Fragment(R.layout.fragment_costs) {
+class IncomeFragment: Fragment(R.layout.fragment_income) {
 
     companion object {
         const val ADD = "add"
     }
 
-    private val binding: FragmentCostsBinding by viewBinding(FragmentCostsBinding::bind)
+    private val binding: FragmentIncomeBinding by viewBinding(FragmentIncomeBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -24,5 +24,4 @@ class CostsFragment: Fragment(R.layout.fragment_costs) {
             navigate(FragmentScreen(AddNoteFragment(), ADD))
         }
     }
-
 }
--- app/src/main/java/com/vlad/finboard/tab/TabFragment.kt
+++ app/src/main/java/com/vlad/finboard/tab/TabFragment.kt
@@ -5,13 +5,22 @@ import android.view.View
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
+import com.google.android.material.tabs.TabLayout.Tab
 import com.vlad.finboard.R
 import com.vlad.finboard.databinding.FragmentTabBinding
 import com.vlad.finboard.navigation.Navigator
 import com.vlad.finboard.navigation.NavigatorHolder
 import com.vlad.finboard.navigation.TabFragmentNavigator
+import com.vlad.finboard.navigation.screen.FragmentScreen
+import com.vlad.finboard.navigation.screen.TabScreen
 
 class TabFragment : Fragment(R.layout.fragment_tab), NavigatorHolder {
+
+    companion object {
+        const val COSTS = "costs"
+        const val INCOME = "income"
+    }
 
     private val binding: FragmentTabBinding by viewBinding(FragmentTabBinding::bind)
     lateinit var navigator: Navigator
@@ -24,12 +33,29 @@ class TabFragment : Fragment(R.layout.fragment_tab), NavigatorHolder {
         super.onCreate(savedInstanceState)
 
         navigator = TabFragmentNavigator(this)
+        if (savedInstanceState == null) {
+            navigator.navigate(TabScreen(FragmentScreen(CostsFragment(), COSTS)))
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         bindOnBackPressedCallback()
+        bindTabLayout()
+    }
+
+    private fun bindTabLayout() {
+        binding.tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
+            override fun onTabSelected(tab: Tab?) {
+                when (tab?.position) {
+                    0 -> navigator.navigate(TabScreen(FragmentScreen(CostsFragment(), COSTS)))
+                    1 -> navigator.navigate(TabScreen(FragmentScreen(IncomeFragment(), INCOME)))
+                }
+            }
+            override fun onTabUnselected(tab: Tab?) {}
+            override fun onTabReselected(tab: Tab?) {}
+        })
     }
 
     private fun bindOnBackPressedCallback() {
